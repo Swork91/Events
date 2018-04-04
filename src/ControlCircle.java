38e7178b2f8f33eaefd2745a0bd1dbca5356b107
Swork91 <chicagomike999@gmail.com>
2018-04-04 11:22:38 -0400

    base classes added and constructors working

--- src/ControlCircle.java
+++ src/ControlCircle.java
@@ -1,19 +1,24 @@
 import javax.swing.*;
-
-import clockStuff.DisplayClock;
-
 import java.awt.*;
 
 public class ControlCircle extends JFrame{
-
+	private JButton jbtEnlarge = new JButton("Enlarge");
+	private JButton jbtShrink = new JButton("Shrink");
+	private CirclePanel canvas = new CirclePanel();
+	
 	public ControlCircle() {
-		// TODO Auto-generated constructor stub
+		JPanel panel = new JPanel();
+		panel.add(jbtEnlarge);
+		panel.add(jbtShrink);
+		
+		this.add(canvas, BorderLayout.CENTER);
+		this.add(panel, BorderLayout.SOUTH);
 	}
 
 	public static void main(String[] args) {
 		ControlCircle frame = new ControlCircle();
 		frame.setTitle("ControlCircle1");
-		frame.setSize(400, 400);
+		frame.setSize(300, 300);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setVisible(true);
