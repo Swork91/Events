bc0358a1d4f7835f6a9e65568fc25192dbe6ad7b
Swork91 <chicagomike999@gmail.com>
2018-04-04 11:09:59 -0400

    initial commit

---
+++ src/ControlCircle.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+import clockStuff.DisplayClock;
+
+import java.awt.*;
+
+public class ControlCircle extends JFrame{
+
+	public ControlCircle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		ControlCircle frame = new ControlCircle();
+		frame.setTitle("ControlCircle1");
+		frame.setSize(400, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
