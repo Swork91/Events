867d1843574e94f1dcfcb1dfe2251a5bd959da23
Swork91 <chicagomike999@gmail.com>
2018-04-04 11:37:59 -0400

    Changed to inner classses for simplicity.

--- src/ControlCircle.java
+++ src/ControlCircle.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ControlCircle extends JFrame{
 	private JButton jbtEnlarge = new JButton("Enlarge");
@@ -13,16 +15,39 @@ public class ControlCircle extends JFrame{
 		
 		this.add(canvas, BorderLayout.CENTER);
 		this.add(panel, BorderLayout.SOUTH);
+		
+		jbtEnlarge.addActionListener(new EnlargeListener());
 	}
 
 	public static void main(String[] args) {
 		ControlCircle frame = new ControlCircle();
-		frame.setTitle("ControlCircle1");
+		frame.setTitle("ControlCircle1+2");
 		frame.setSize(300, 300);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setVisible(true);
 
 	}
-
+	
+	//inner class
+	class CirclePanel extends JPanel{
+		private int radius = 5;
+		
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawOval(getWidth()/2-radius, getHeight()/2-radius, 2*radius, 2*radius);
+		}
+		
+		public void enlarge() {
+			radius+=2;
+			repaint();
+		}
+	}
+	
+	//inner class
+	class EnlargeListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			canvas.enlarge();
+		}
+	}
 }
